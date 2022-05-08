9e9d2fd959bdebc3b401199b722e861e23c54e6c
moanrisy <moanrisy@gmail.com>
2022-05-08 12:07:25 +0700

    Update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
@@ -11,19 +15,62 @@ export class AuthService {
     // generate the password hars
     const hash = await argon.hash(dto.password);
     // save the user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-    // return the saved user
-    return user;
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (err) {
+      if (
+        err instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+
+      throw err;
+    }
   }
 
-  signin() {
-    return { msg: 'I am signin in' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Crredentials incorrect',
+      );
+
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Crredentials incorrect',
+      );
+
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
