3284750890768fcfabe7f1a6cb0db84b782c3ed3
moanrisy <moanrisy@gmail.com>
2022-05-09 11:41:41 +0700

    Add custom decorator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -89,7 +89,7 @@ export class AuthService {
     const token = await this.jwt.signAsync(
       payload,
       {
-        expiresIn: '15m',
+        expiresIn: '3h',
         secret: secret,
       },
     );
